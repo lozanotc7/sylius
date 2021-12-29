5fb14b024bcbfc37a55f5d35a272aea234e3a6da
Rodrigo Lozano <rodrigo.lozano@acobur.es>
2021-12-29 23:44:33 +0100

    init

---
+++ src/Entity/Customer/Customer.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Customer;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Core\Model\Customer as BaseCustomer;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Currency\Model\CurrencyInterface;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_customer")
+ */
+class Customer extends BaseCustomer
+{
+    /**
+     * @var Collection|NewsletterInterface[]
+     *
+     * @psalm-var Collection<array-key, NewsletterInterface>
+     */
+    protected $newsletters;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        /** @var ArrayCollection<array-key, NewsletterInterface> $this->newsletters */
+        $this->newsletters = new ArrayCollection();
+    }
+
+    public function getNewsletters(): Collection
+    {
+        return $this->newsletters;
+    }
+
+    public function addNewsletter(NewsletterInterface $newsletter): void
+    {
+        if (!$this->hasNewsletter($newsletter)) {
+            $this->newsletters->add($newsletter);
+        }
+    }
+
+    public function removeNewsletter(NewsletterInterface $newsletter): void
+    {
+        if ($this->hasNewsletter($newsletter)) {
+            $this->newsletters->removeElement($newsletter);
+        }
+    }
+
+    public function hasNewsletter(NewsletterInterface $newsletter): bool
+    {
+        return $this->newsletters->contains($newsletter);
+    }
+}
